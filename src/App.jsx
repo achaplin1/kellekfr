5451ad3cc223d29616e9dfbfa7da5a8ce91067f1
achaplin1 <85441727+achaplin1@users.noreply.github.com>
2025-06-17 21:07:45 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import { motion } from 'framer-motion';
 
-const socket = io('http://localhost:3001');
+const socket = io('kollek-production.up.railway.app');
 
 const sampleQuestions = [
   "Qui adore les chats ?",
